521cea8eea0731f1114941520a6f3a2ee47974da
Tomas Zikmund <tommekster@gmail.com>
2022-01-24 20:20:11 +0100

    react bootstrap a menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, Nav } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+      <Nav variant="tabs" defaultActiveKey="switches">
+        <Nav.Item>
+          <Nav.Link eventKey="switches">Switches</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="captive">Captive portal</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <Container></Container>
+    </Container>
   );
 }
 
