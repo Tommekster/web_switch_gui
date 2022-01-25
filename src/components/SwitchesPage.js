afd24fe77b1a3feb8e0c09711034427ae8da39e3
Tomas Zikmund <tommekster@gmail.com>
2022-01-25 09:42:25 +0100

    switches page - call api gui feedback

--- src/components/SwitchesPage.js
+++ src/components/SwitchesPage.js
@@ -1,21 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import { ListGroup, FormCheck, Stack, Spinner } from "react-bootstrap";
 import * as api from "../api/switchesApi";
 
+function switchesReducer(switches, action) {
+  switch (action.type) {
+    case "loading":
+      return switches.map((x) =>
+        x.id === action.switchId ? { ...x, loading: true } : x
+      );
+    case "update":
+      return switches.map((x) =>
+        x.id === action.switch.id ? { ...action.switch, loading: false } : x
+      );
+    case "set":
+      return action.switches;
+    default:
+      throw new Error();
+  }
+}
+
 function Switches(props) {
-  const [switches, setSwitches] = useState([]);
+  const [switches, dispatch] = useReducer(switchesReducer, []);
+  const setSwitches = (switches) => dispatch({ type: "set", switches });
+  const setLoading = (switchId) => dispatch({ type: "loading", switchId });
+  const updateSwitch = (sw) => dispatch({ type: "update", switch: sw });
 
   useEffect(() => {
     if (switches.length === 0) {
-      api.getSwitches().then((x) => setSwitches(x));
+      api
+        .getSwitches()
+        .then((switches) =>
+          setSwitches(switches.map((x) => ({ ...x, loading: false })))
+        );
     }
-  }, []);
+  }, [switches.length]);
 
-  const updateSwitch = (sw) => {
-    const newSwitch = { ...sw, switched: !sw.switched };
-    const newSwitches = switches.map((x) => (x.id === sw.id ? newSwitch : x));
-    api.saveSwitch(newSwitch);
-    setSwitches(newSwitches);
+  const onSwitchChanged = (e, switchId) => {
+    setLoading(switchId);
+    const sw = switches.find((x) => x.id === switchId);
+    const switched = e.target.checked;
+    var updated = { ...sw, switched };
+    api.saveSwitch(updated).then((x) => updateSwitch(x));
   };
 
   return (
@@ -30,12 +55,17 @@ function Switches(props) {
       <ListGroup>
         {switches.map((x) => (
           <ListGroup.Item key={x.id}>
-            <FormCheck
-              type="switch"
-              label={x.label}
-              checked={x.switched}
-              onClick={() => updateSwitch(x)}
-            />
+            <Stack direction="horizontal" gap={3}>
+              <FormCheck
+                type="switch"
+                id={`switch-${x.id}`}
+                label={x.label}
+                checked={x.switched}
+                disabled={x.loading}
+                onChange={(e) => onSwitchChanged(e, x.id)}
+              />
+              {x.loading && <Spinner animation="border" size="sm" />}
+            </Stack>
           </ListGroup.Item>
         ))}
       </ListGroup>
