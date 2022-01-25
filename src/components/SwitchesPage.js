34d07ac1a7c7d9a1d8f3674c1dbcba1e0a125d76
Tomas Zikmund <tommekster@gmail.com>
2022-01-25 08:49:57 +0100

    switches page - calling API

--- src/components/SwitchesPage.js
+++ src/components/SwitchesPage.js
@@ -1,20 +1,45 @@
-import React from "react";
-import { ListGroup, FormCheck } from "react-bootstrap";
-
-const switches = [
-  { id: "0", label: "Swicth 1" },
-  { id: "1", label: "Swicth 2" },
-];
+import React, { useState, useEffect } from "react";
+import { ListGroup, FormCheck, Stack, Spinner } from "react-bootstrap";
+import * as api from "../api/switchesApi";
 
 function Switches(props) {
+  const [switches, setSwitches] = useState([]);
+
+  useEffect(() => {
+    if (switches.length === 0) {
+      api.getSwitches().then((x) => setSwitches(x));
+    }
+  }, []);
+
+  const updateSwitch = (sw) => {
+    const newSwitch = { ...sw, switched: !sw.switched };
+    const newSwitches = switches.map((x) => (x.id === sw.id ? newSwitch : x));
+    api.saveSwitch(newSwitch);
+    setSwitches(newSwitches);
+  };
+
   return (
-    <ListGroup>
-      {switches.map((x) => (
-        <ListGroup.Item key={x.id}>
-          <FormCheck type="switch" label={x.label} />
-        </ListGroup.Item>
-      ))}
-    </ListGroup>
+    <Stack>
+      {switches.length === 0 && (
+        <Spinner
+          className="col-md-5 mx-auto"
+          variant="primary"
+          animation="grow"
+        />
+      )}
+      <ListGroup>
+        {switches.map((x) => (
+          <ListGroup.Item key={x.id}>
+            <FormCheck
+              type="switch"
+              label={x.label}
+              checked={x.switched}
+              onClick={() => updateSwitch(x)}
+            />
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Stack>
   );
 }
 
