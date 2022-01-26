8bc2c6f16bd428639ff1226070079e48dd9a9f22
Tomas Zikmund <tommekster@gmail.com>
2022-01-26 21:55:50 +0100

    show login page not logged user

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import "./App.css";
-import { Container, Tabs, Tab } from "react-bootstrap";
+import { Container, Tabs } from "react-bootstrap";
+import { UserContext } from "./hooks/UserContext";
+import useUser from "./hooks/useUser";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
 import SwitchesPage from "./components/SwitchesPage";
 import CaptivePortalPage from "./components/CaptivePortalPage";
 
 function App() {
+  const _useUser = useUser();
+  const { isLoggedIn } = _useUser;
   return (
-    <Container className="p-3">
-      <Tabs defaultActiveKey="switches" className="mb-3">
-        <Tab eventKey="switches" title="Switches">
-          <SwitchesPage />
-        </Tab>
-        <Tab eventKey="captive" title="Captive portal">
-          <CaptivePortalPage />
-        </Tab>
-      </Tabs>
-    </Container>
+    <UserContext.Provider value={{ ..._useUser }}>
+      <Container className="p-3 mb-3">
+        {!isLoggedIn && (
+          <Tabs defaultActiveKey="login-in">
+            <Tabs.Tab eventKey="login-in" title="Login">
+              <LoginPage />
+            </Tabs.Tab>
+            <Tabs.Tab eventKey="sign-up" title="Sign up">
+              <SignUpPage />
+            </Tabs.Tab>
+          </Tabs>
+        )}
+        {isLoggedIn && (
+          <Tabs defaultActiveKey="captive">
+            <Tabs.Tab eventKey="switches" title="Switches">
+              <SwitchesPage />
+            </Tabs.Tab>
+            <Tabs.Tab eventKey="captive" title="Captive portal">
+              <CaptivePortalPage />
+            </Tabs.Tab>
+          </Tabs>
+        )}
+      </Container>
+    </UserContext.Provider>
   );
 }
 
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,5 +3,6 @@ import { useState } from "react";
 export default function useUser() {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState();
-  return { token, user, setUser, setToken };
+  const isLoggedIn = token && user;
+  return { isLoggedIn, token, user, setUser, setToken };
 }
