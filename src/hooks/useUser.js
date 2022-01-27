a2d9e6715cde80785033203b9952079700ef9591
Tomas Zikmund <tommekster@gmail.com>
2022-01-27 20:11:19 +0100

    save token & user to localStorage

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -1,13 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { UnauthorizedError } from "../api/apiUtils";
 
 export default function useUser() {
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const { user, token } = JSON.parse(localStorage.getItem("user"));
+    setUser(user);
+    setToken(token);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify({ token, user }));
+  }, [token, user]);
+
   const isLoggedIn = token && user;
   const logout = () => {
     setToken(null);
     setUser(null);
+    localStorage.removeItem("user");
   };
   const handleUnauthorized = (error) => {
     if (error instanceof UnauthorizedError) {
